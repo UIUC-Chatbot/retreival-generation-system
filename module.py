05d5dbcd0c322612d4233ae6b74378d4b2512a5f
nehasheth2 <nehasheth1998@gmail.com>
2022-12-21 14:42:16 -0800

    added prompting

--- module.py
+++ module.py
@@ -71,8 +71,29 @@ class opt_model():
         outputs = self.model.generate(**inputs,penalty_alpha=0.6, top_k = 4,max_length=max_len)
         out_text = self.tokenizer.batch_decode(outputs,skip_special_tokens = True)[0]
         return out_text
+    def prepare_prompt(self, context:str,question:str) -> str :
+        """prepares prompt based on type of question - factoid, causal or listing"""
+        factoid = ["What", "Where", "When", "Explain", "Discuss", "Clarify"]
+        causal = ["Why", "How"]
+        listing = ["List", "Break down"]
+        if any(word in question for word in factoid):
+            prompt = """Generate an objective, formal and logically sound answer to this question, based on the given context. 
+            The answer must spur curiosity, enable interactive discussions and make the user ask further questions. 
+            It should be interesting and use advanced vocabulary and complex sentence structures.
+            Context : """ + context.replace("\n"," ") + "\nQuestion:" + question.replace("\n"," ") + "\nAnswer:"
+        elif any(word in question for word in causal):
+            prompt = """Generate a procedural, knowledgeable and reasoning-based answer about this question, based on the given context. 
+            The answer must use inference mechanisms and logic to subjectively discuss the topic. It should be creative and logic-oriented, analytical and extensive. Context :""" + context.replace("\n"," ") + "\nQuestion:" + question.replace("\n"," ") + "\nAnswer:"
+        elif any(word in question for word in listing):
+            prompt = """Generate a list-type, descriptive answer to this question, based on the given context. 
+            The answer should be very detailed and contain reasons, explanations and elaborations about the topic. It should be interesting and use advanced vocabulary and complex sentence structures. Context :""" + context.replace("\n"," ") + "\nQuestion:" + question.replace("\n"," ") + "\nAnswer:"
+        else:
+            prompt = """Generate a detailed, interesting answer to this question, based on the given context. 
+            The answer must be engaging and provoke interactions. It should use academic language and a formal tone. 
+            Context : """ + context.replace("\n"," ") + "\nQuestion:" + question.replace("\n"," ") + "\nAnswer:"
+        return prompt
     def answer_question(self,context:str,question:str,max_len:int = 300):
-        prompt = "Answer question from context:" + context.replace("\n"," ") + "\nQuestion:" + question.replace("\n"," ") + "\nAnswer:"
+        prompt = self.prepare_prompt(context, question)
         return self.text_gen(prompt,max_len).split("\nAnswer:")[1]
     def train_loss_ids(self,input_ids,label_ids):
         data = input_ids.to(self.device)
